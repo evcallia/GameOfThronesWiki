6762c613ee17d8b0072ea33df42c98caeea869e2
Evan <calliaevan12@gmail.com>
2016-09-22 08:34:32 -0700

    added search bar

--- GameOfThrones_HouseIndex/ListOfHousesTableViewController.swift
+++ GameOfThrones_HouseIndex/ListOfHousesTableViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ListOfHousesTableViewController: UITableViewController {
+class ListOfHousesTableViewController: UITableViewController, UISearchResultsUpdating {
+    
     var houses = [House]()
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredHouses = [House]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,17 +25,46 @@ class ListOfHousesTableViewController: UITableViewController {
             })
         })
         self.navigationItem.title = "Houses"
+        
+        //search stuff
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
     }
 
+// MARK: - Search functionality
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+    
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        filteredHouses = houses.filter {
+            house in
+            return house.name.lowercased().contains(searchText.lowercased())
+        }
+        tableView.reloadData()
+    }
+//**********
+    
+    
 // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return filteredHouses.count
+        }
         return houses.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HouseCell", for: indexPath)
-        cell.textLabel?.text = houses[indexPath.row].name
+        let house: House
+        if searchController.isActive && searchController.searchBar.text != "" {
+            house = filteredHouses[indexPath.row]
+        } else {
+            house = houses[indexPath.row]
+        }
+        cell.textLabel?.text = house.name
         return cell
     }
     
